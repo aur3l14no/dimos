b8e2010e84b9812dda24e67b593666fed747f813
aur3l14no <muzhi.yu1@gmail.com>
2026-07-16 10:15:10 +0800

    build(raytracing): cache Rust dependencies with Crane

--- dimos/mapping/ray_tracing/rust/flake.nix
+++ dimos/mapping/ray_tracing/rust/flake.nix
@@ -4,36 +4,97 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    dimos-rust = { url = "path:../../../../native/rust"; flake = false; };
+    crane.url = "github:ipetkov/crane";
+    dimos-rust = {
+      url = "path:../../../../native/rust";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, dimos-rust }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      crane,
+      dimos-rust,
+      flake-utils,
+      nixpkgs,
+      self,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
+        craneLib = crane.mkLib pkgs;
+        lib = pkgs.lib;
+        python = pkgs.python312;
 
-        src = pkgs.runCommand "voxel-ray-tracing-src" {} ''
-          mkdir -p $out/dimos/mapping/ray_tracing/rust
-          cp -r ${./src} $out/dimos/mapping/ray_tracing/rust/src
-          cp ${./Cargo.toml} $out/dimos/mapping/ray_tracing/rust/Cargo.toml
-          cp ${./Cargo.lock} $out/dimos/mapping/ray_tracing/rust/Cargo.lock
+        dimosRustRoot = toString dimos-rust;
+        dimosRustSrc = lib.cleanSourceWith {
+          src = dimos-rust;
+          name = "dimos-rust-source";
+          filter =
+            path: type:
+            let
+              rel = lib.removePrefix "${dimosRustRoot}/" (toString path);
+              inCrate = crate: rel == crate || lib.hasPrefix "${crate}/" rel;
+            in
+            rel == "Cargo.toml"
+            || (
+              (inCrate "dimos-module" || inCrate "dimos-module-macros")
+              && craneLib.filterCargoSources path type
+            );
+        };
+        crateSrc = pkgs.lib.fileset.toSource {
+          root = ./.;
+          fileset = craneLib.fileset.commonCargoSources ./.;
+        };
 
-          mkdir -p $out/native/rust
-          cp -r ${dimos-rust}/dimos-module $out/native/rust/dimos-module
-          cp -r ${dimos-rust}/dimos-module-macros $out/native/rust/dimos-module-macros
-        '';
-      in {
-        packages.default = pkgs.rustPlatform.buildRustPackage {
+        commonArgs = {
           pname = "voxel-ray-tracing";
           version = "0.1.0";
+          src = crateSrc;
 
-          inherit src;
-          cargoRoot = "dimos/mapping/ray_tracing/rust";
-          buildAndTestSubdir = "dimos/mapping/ray_tracing/rust";
-
-          cargoHash = "sha256-0d0dlNDvDplA7oWTyUWOCOlS74Zie8uMQ+ps6lXntOI=";
-
-          meta.mainProgram = "voxel_ray_tracing";
+          cargoLock = ./Cargo.lock;
+          cargoToml = ./Cargo.toml;
+          RUSTFLAGS = pkgs.lib.optionalString pkgs.stdenv.isDarwin (
+            "-C link-arg=-undefined -C link-arg=dynamic_lookup"
+          );
+          postPatch = ''
+            substituteInPlace Cargo.toml \
+              --replace-fail '../../../../native/rust/dimos-module' '${dimosRustSrc}/dimos-module'
+          '';
+          strictDeps = true;
         };
-      });
+
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        package = craneLib.buildPackage (
+          commonArgs
+          // {
+            inherit cargoArtifacts;
+            doCheck = false;
+            postInstall = ''
+              sitePackages=$out/${python.sitePackages}
+              mkdir -p "$sitePackages"
+              cp target/release/libdimos_voxel_ray_tracing${pkgs.stdenv.hostPlatform.extensions.sharedLibrary} \
+                "$sitePackages/dimos_voxel_ray_tracing.abi3.so"
+              PYTHONPATH="$sitePackages" ${python.interpreter} -c \
+                "from dimos_voxel_ray_tracing import VoxelRayMapper"
+            '';
+            meta.mainProgram = "voxel_ray_tracing";
+          }
+        );
+        tests = craneLib.cargoTest (
+          commonArgs
+          // {
+            inherit cargoArtifacts;
+          }
+        );
+      in
+      {
+        packages.default = package;
+        checks = {
+          build = package;
+          inherit tests;
+        };
+      }
+    );
 }
